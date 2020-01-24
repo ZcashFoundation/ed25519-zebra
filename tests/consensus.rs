6168c0cced0128f20894a800a6ceb35f1fa3621d
Henry de Valence <hdevalence@hdevalence.ca>
2020-01-23 17:31:36 -0800

    Stub out some edge cases to test.

---
+++ tests/consensus.rs
@@ -0,0 +1,37 @@
+//! Tests for consensus-critical validation rules.
+
+#[test]
+fn reject_non_canonical_field_encoding_of_signature_s() {
+    unimplemented!();
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn accept_non_canonical_field_encoding_of_signature_R() {
+    unimplemented!();
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn reject_low_order_signature_R() {
+    unimplemented!();
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn accept_torsion_component_in_signature_R() {
+    unimplemented!();
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn accept_non_canonical_field_encoding_of_pubkey_A() {
+    unimplemented!();
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn reject_low_order_pubkey_A() {
+    unimplemented!();
+}
+
