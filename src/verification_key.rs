8c97acde89b6446f7f2505069fc7b9ac3a541417
Sean Bowe <ewillbefull@gmail.com>
2020-06-22 13:22:11 -0600

    Change validation and serialization rules to match ZIP 215.

--- src/verification_key.rs
+++ src/verification_key.rs
@@ -6,7 +6,7 @@ use curve25519_dalek::{
 };
 use sha2::{Digest, Sha512};
 
-use crate::{constants, Error, Signature};
+use crate::{Error, Signature};
 
 /// A refinement type for `[u8; 32]` indicating that the bytes represent an
 /// encoding of an Ed25519 verification key.
@@ -69,22 +69,9 @@ impl From<VerificationKeyBytes> for [u8; 32] {
 /// ## Zcash-specific consensus properties
 ///
 /// Ed25519 checks are described in [§5.4.5][ps] of the Zcash protocol
-/// specification. However, it is not clear that the protocol specification
-/// matches the implementation in `libsodium` `1.0.15` used by `zcashd`. Note
-/// that the precise version is important because `libsodium` changed validation
-/// rules in point releases.
+/// specification.
 ///
-/// The spec says that a verification key `A` is
-///
-/// > a point of order `l` on the Ed25519 curve, in the encoding specified by…
-///
-/// but `libsodium` `1.0.15` does not check this. Instead it only checks whether
-/// the encoding of `A` is an encoding of a point on the curve and that the
-/// encoding is not all zeros. This implementation matches the `libsodium`
-/// behavior.  This has implications for signature verification behaviour, as noted
-/// in the [`VerificationKey::verify`] documentation.
-///
-/// [ps]: https://zips.z.cash/protocol/protocol.pdf#concretejssig
+/// [ps]: https://zips.z.cash/protocol/protocol.pdf#concreteed25519
 #[derive(Copy, Clone, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(try_from = "VerificationKeyBytes"))]
@@ -117,13 +104,8 @@ impl TryFrom<VerificationKeyBytes> for VerificationKey {
     type Error = Error;
     #[allow(non_snake_case)]
     fn try_from(bytes: VerificationKeyBytes) -> Result<Self, Self::Error> {
-        // libsodium behavior: public key bytes must not be all 0
-        // libsodium 1.0.15 crypto_sign/ed25519/ref10/open.c:138-143
-        // Note: this is different from the description in the spec.
-        if bytes.0 == [0; 32] {
-            return Err(Error::MalformedPublicKey);
-        }
-
+        // Zcash consensus rule: A MUST represent a point on the Ed25519 curve,
+        // but does not need to be a canonical encoding.
         let A = CompressedEdwardsY(bytes.0)
             .decompress()
             .ok_or(Error::MalformedPublicKey)?;
@@ -149,43 +131,19 @@ impl VerificationKey {
     /// ## Zcash-specific consensus properties
     ///
     /// Ed25519 checks are described in [§5.4.5][ps] of the Zcash protocol
-    /// specification. Ed25519 validation is not well-specified, and the original
-    /// implementation of JoinSplit signatures for `zcashd` inherited its precise
-    /// validation rules from a specific build configuration of `libsodium`
-    /// `1.0.15`. Note that the precise version is important because `libsodium`
-    /// changed validation rules in point releases.
+    /// specification.
     ///
-    /// The additional validation checks are that:
-    ///
-    /// * `s` MUST represent an integer less than the prime `l`, per `libsodium`
-    /// `1.0.15` `crypto_sign/ed25519/ref10/open.c:126`;
-    ///
-    /// * `R` MUST NOT be one of the excluded encodings, per `libsodium` `1.0.15`
-    /// `crypto_sign/ed25519/ref10/open.c:127`;
-    ///
-    /// * The public key bytes must not be all 0, per `libsodium` `1.0.15`
-    /// `crypto_sign/ed25519/ref10/open.c:138-143`, which we maintain as an
-    /// invariant on the `VerificationKey` type.
-    ///
-    /// [ps]: https://zips.z.cash/protocol/protocol.pdf#concretejssig
+    /// [ps]: https://zips.z.cash/protocol/protocol.pdf#concreteed25519
     #[allow(non_snake_case)]
     pub fn verify(&self, signature: &Signature, msg: &[u8]) -> Result<(), Error> {
         // Zcash consensus rule: `s` MUST represent an integer less than the prime `l`.
-        // libsodium 1.0.15 crypto_sign/ed25519/ref10/open.c:126
         let s = Scalar::from_canonical_bytes(signature.s_bytes).ok_or(Error::InvalidSignature)?;
 
-        // Zcash consensus rule: `R` MUST NOT be one of the encodings excluded by libsodium 1.0.15
-        // libsodium 1.0.15 crypto_sign/ed25519/ref10/open.c:127
-        for excluded in &constants::EXCLUDED_POINT_ENCODINGS {
-            if &signature.R_bytes == excluded {
-                return Err(Error::InvalidSignature);
-            }
-        }
-
-        // libsodium behavior: public key bytes must not be all 0
-        // libsodium 1.0.15 crypto_sign/ed25519/ref10/open.c:138-143
-        // Note: this is different from the description in the spec.
-        // No-op, since we maintain this invariant in the PublicKey type.
+        // Zcash consensus rule: R MUST represent a point on the Ed25519 curve,
+        // but does not need to be a canonical encoding.
+        let R = CompressedEdwardsY(signature.R_bytes)
+            .decompress()
+            .ok_or(Error::InvalidSignature)?;
 
         let k = Scalar::from_hash(
             Sha512::default()
@@ -195,9 +153,10 @@ impl VerificationKey {
         );
 
         // We expect to recompute R as [s]B - [k]A = [k](-A) + [s]B.
-        let R = EdwardsPoint::vartime_double_scalar_mul_basepoint(&k, &self.minus_A, &s);
+        let recomputed_R = EdwardsPoint::vartime_double_scalar_mul_basepoint(&k, &self.minus_A, &s);
 
-        if R.compress().to_bytes() == signature.R_bytes {
+        // Check whether [8]R = [8]([s]B - [k]A), i.e., whether R - ([s]B - [k]A) is of small order.
+        if (R - recomputed_R).is_small_order() {
             Ok(())
         } else {
             Err(Error::InvalidSignature)
