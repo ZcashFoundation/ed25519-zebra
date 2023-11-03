c3cc9abc27ea22e07a6c2f98c0eb7dcb70139e69
Justin Abrahms <justin@abrah.ms>
2023-11-03 10:56:45 -0700

    Implement parital equality for key types
    Signed-off-by: Justin Abrahms <justin@abrah.ms>

--- src/signing_key.rs
+++ src/signing_key.rs
@@ -43,7 +43,7 @@ use crate::{VerificationKey, VerificationKeyBytes};
 /// An Ed25519 signing key.
 ///
 /// This is also called a secret key by other implementations.
-#[derive(Copy, Clone, Zeroize)]
+#[derive(PartialEq, Copy, Clone, Zeroize)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(from = "SerdeHelper"))]
 #[cfg_attr(feature = "serde", serde(into = "SerdeHelper"))]
--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -11,31 +11,28 @@ fn parsing() {
     let pkb = VerificationKeyBytes::from(&sk);
     let sig = sk.sign(b"test");
 
-    // Most of these types don't implement Eq, so we check a round trip
-    // conversion to bytes, using these as the reference points:
-
     let sk_array: [u8; 32] = sk.into();
     let pk_array: [u8; 32] = pk.into();
     let pkb_array: [u8; 32] = pkb.into();
     let sig_array: [u8; 64] = sig.into();
 
-    let sk2 = SigningKey::try_from(sk.as_ref()).unwrap();
-    let pk2 = VerificationKey::try_from(pk.as_ref()).unwrap();
-    let pkb2 = VerificationKeyBytes::try_from(pkb.as_ref()).unwrap();
-    let sig2 = Signature::try_from(<[u8; 64]>::from(sig).as_ref()).unwrap();
+    let sk2 = SigningKey::try_from(sk_array).unwrap();
+    let pk2 = VerificationKey::try_from(pk_array).unwrap();
+    let pkb2 = VerificationKeyBytes::try_from(pkb_array).unwrap();
+    let sig2 = Signature::try_from(sig_array).unwrap();
 
-    assert_eq!(&sk_array[..], sk2.as_ref());
-    assert_eq!(&pk_array[..], pk2.as_ref());
-    assert_eq!(&pkb_array[..], pkb2.as_ref());
-    assert_eq!(&sig_array[..], <[u8; 64]>::from(sig2).as_ref());
+    assert_eq!(sk, sk2);
+    assert_eq!(pk, pk2);
+    assert_eq!(pkb, pkb2);
+    assert_eq!(sig, sig2);
 
     let sk3: SigningKey = bincode::deserialize(sk.as_ref()).unwrap();
     let pk3: VerificationKey = bincode::deserialize(pk.as_ref()).unwrap();
     let pkb3: VerificationKeyBytes = bincode::deserialize(pkb.as_ref()).unwrap();
 
-    assert_eq!(&sk_array[..], sk3.as_ref());
-    assert_eq!(&pk_array[..], pk3.as_ref());
-    assert_eq!(&pkb_array[..], pkb3.as_ref());
+    assert_eq!(sk, sk3);
+    assert_eq!(pk, pk3);
+    assert_eq!(pkb, pkb3);
 }
 
 #[test]
